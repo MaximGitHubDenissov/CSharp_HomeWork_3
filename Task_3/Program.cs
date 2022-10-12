bc4ad00f6b94fc3db232f14e479b6e94b10dad1f
Maxim Denissov <DM7894289@gmail.com>
2022-10-12 14:18:31 +0600

    Add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Ввведите число N больше единицы");
+int N = int.Parse(Console.ReadLine()!);
+int M = 1;
+
+if (N < 1)
+{
+    Console.WriteLine("Вы ввели некорректное число N");
+}
+else
+{
+    while (M < N)
+    {
+        Console.Write($"{Math.Pow(M, 3)}, ");
+        M ++;
+    }
+Console.Write($"{Math.Pow(M, 3)} ");
+}
